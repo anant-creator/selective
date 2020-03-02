e7acc5d8908f50bac6a67e649a40bb5faa5ec30c
anant-creator <61108547+anant-creator@users.noreply.github.com>
2020-03-02 23:34:17 +0530

    Add files via upload

---
+++ sar.py
@@ -0,0 +1,25 @@
+class Girl(): 
+    def __init__(self, name, height, complection, eyes, lips, hobbies):
+        self.name = name
+        self.height = height
+        self.complection = comp
+        self.eyes = eyes
+        self.lips = lips
+        self.hobbies = hobbies
+
+    def intro():
+        print("Hello There!")
+        print("My name is ", self.name)
+        print("My height is ", self.height)(
+        print("My skin color is ", self.comp)
+        print("The color of my eyes is", self.eyes)
+        print("My lips are", self.lips)
+        print("My hobbies are", self.hobbies)
+
+
+s1 = Girl("Sharda", "5.4ft", "fair", "brown", "pink", "Drawing & Dancing")
+
+s1.intro()
+
+
+
